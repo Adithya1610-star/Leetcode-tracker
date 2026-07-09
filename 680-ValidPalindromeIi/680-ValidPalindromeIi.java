35661b52409b6ea4a94a8ea05a79f893f15ba60d
Adithya1610-star <23cs002@drngpit.ac.in>
2026-07-09 09:14:35 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.java
@@ -0,0 +1,36 @@
+// Last updated: 7/9/2026, 9:14:35 AM
+class Solution {
+    public boolean validPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        int i = 0, j = sb.length() - 1;
+
+        while (i < j) {
+            if (sb.charAt(i) != sb.charAt(j)) {
+             
+                sb.deleteCharAt(i);
+                if (isPalindrome(sb)) return true;
+                sb.insert(i, s.charAt(i));
+               
+                sb.deleteCharAt(j);
+                if (isPalindrome(sb)) return true;
+                sb.insert(j, s.charAt(j)); 
+
+                return false; 
+            }
+            i++;
+            j--;
+        
+        }
+        return true; 
+    
+    }
+    boolean isPalindrome(StringBuilder sb) {
+        int left = 0, right = sb.length() - 1;
+        while (left < right) {
+            if (sb.charAt(left) != sb.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
