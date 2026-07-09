248d747d9599ad65de29aca402d79ea551763a46
Adithya1610-star <23cs002@drngpit.ac.in>
2026-07-09 09:15:01 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:15:01 AM
+class Solution {
+    public int firstUniqChar(String s) {
+      char st[]=s.toCharArray();
+      HashMap<Character,Integer>map=new HashMap<>();
+      for(char c:st){
+        map.put(c,map.getOrDefault(c,0)+1);
+      }
+        for (int i = 0; i < st.length; i++) {
+            if (map.get(st[i]) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
