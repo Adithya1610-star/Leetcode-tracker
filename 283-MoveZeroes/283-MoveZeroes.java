079305949ce517bef51d5fdd72726d93e02f1ee3
Adithya1610-star <23cs002@drngpit.ac.in>
2026-07-09 09:15:11 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:15:11 AM
+class Solution{ 
+    public void moveZeroes(int[] nums) {
+    if (nums == null || nums.length == 0) return;        
+
+    int insertPos = 0;
+    for (int num: nums) {
+        if (num != 0) 
+            nums[insertPos++] = num;
+    }        
+
+    while (insertPos < nums.length) {
+        nums[insertPos++] = 0;
+    }
+    }
+}
